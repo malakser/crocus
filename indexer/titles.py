e54429366dc2ade7e0af69518743a9907df38057
Teletrup <saletrapotasowatosyf@gmail.com>
2022-09-25 21:52:57 +0200

    blargh

--- indexer/titles.py
+++ indexer/titles.py
@@ -1,22 +1,26 @@
 import requests
 import jsons 
+import re
 from bs4 import BeautifulSoup
+
 
 with open('../data/legit100.json') as f:
   sites = jsons.loads(f.read())
 
-
+def word_clean(word):
+  return re.sub(r'[\\\[\]~`!@#$%^&*()+={}:"|;,./<>?]', '', word[:100])
 
 def pull_stuff(url):
   ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30'
   hs = {'User-Agent':ua, 'Range':'bytes=0-1000000'}
-  resp = requests.get(url, timeout=0.5, headers=hs)
+  resp = requests.get(url, headers=hs)
   soup = BeautifulSoup(resp.content, 'html.parser')
-  title = soup.title.text.split().strip()
-  body = soup.body.text.split().strip()
-  res = {'title':title, 'body':body}
+  title = soup.title.text 
+  title_words = [word_clean(s) for s in title.split()]
+  #body_words = [word_clean(s) for s in soup.body.text.split()]
+  res = {'title':title, 'title_words':title_words}
   print(res)
   return res
 
-titles = {s['url']:pull_stuff(s['url']) for s in sites}
+raw = {s['url']:pull_stuff(s['url']) for s in sites}
 

54b53143c437a8e70d930ba075f5fba04d017ead
Teletrup <saletrapotasowatosyf@gmail.com>
2023-04-11 22:33:41 +0200

    header_received wrong type handler

--- search2/crawl/tutorial/tutorial/spiders/qs.py
+++ search2/crawl/tutorial/tutorial/spiders/qs.py
@@ -22,9 +22,9 @@ def is_followable(link, logger):
   schemes = {'https', 'http', 'ftp', ''}
   if props.scheme not in schemes:
     return False
-  if props.netloc in blacklist:
+  if props.netloc in blacklist: #TODO move to appropriate signal to cancel already queued requests
     logger.info(f'{link} is blacklisted')
-    return False
+    return False 
   return True
   
 
@@ -32,28 +32,35 @@ def is_followable(link, logger):
 class QuotesSpider(scrapy.Spider):
   name = "boo"
   start_urls = ['https://ranprieur.com']
+
+  @classmethod
+  def from_crawler(cls, crawler):
+    spider = super().from_crawler(crawler)
+    crawler.signals.connect(spider.headers_received, signal=scrapy.signals.headers_received)
+    return spider
+
+  def headers_received(self, headers, body_length, request, spider):
+    if 'robots.txt' not in request.url and b'text/html' not in headers['content-type']:
+      self.logger.info(f'{request.url} - not HTML')
+      raise scrapy.exceptions.StopDownload(fail=False)
+
   def parse(self, res):
     props = urllib.parse.urlparse(res.url)
     self.logger.info(f'crawling {res.url}')
-    if b'text/html' not in res.headers['content-type']:
-      self.logger.info(f'{res.url} - not HTML')
-      return
-
     if res.css('script') or res.css('iframe'):
       self.logger.info(f'blacklisting {props.netloc}')
       blacklist.add(props.netloc)
       return
-      
     links = list(map(lambda l: urllib.parse.urljoin(res.url, l), res.css('a::attr(href)').getall()))
     #sanitize links here? don't want javascript::stuff
     yield {
       'url': res.url,
       'links': links,
     }
-
     for link in links:
       #defragging?
       if is_followable(link, self.logger):
         yield res.follow(link, callback=self.parse)
+
   def closed(self, reason):
     print(blacklist)

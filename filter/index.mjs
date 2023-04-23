126a7b446dabb34c23b6dcf9bd3f09a566d43a33
teletrup <saletrapotasowatosyf@gmail.com>
2023-04-24 00:27:01 +0200

    started filtering

--- filter/index.mjs
+++ filter/index.mjs
@@ -1,8 +1,10 @@
 import { fullLists, PuppeteerBlocker, Request } from '@cliqz/adblocker-puppeteer';
-import { promises as fs } from 'fs';
+import * as fs from 'fs';
 import * as puppeteer from 'puppeteer';
+import * as readline from 'readline';
 
-//can I do without the outer async?
+
+
 const blocker = await PuppeteerBlocker.fromLists(
   fetch, 
   fullLists,
@@ -11,8 +13,8 @@ const blocker = await PuppeteerBlocker.fromLists(
   },
   {
     path: 'engine.bin',
-    read: fs.readFile,
-    write: fs.writeFile,
+    read: fs.promises.readFile,
+    write: fs.promises.writeFile,
   },
 );
 
@@ -21,4 +23,54 @@ const browser = await puppeteer.launch({
   headless: false,
 });
 
+async function isLegit(url) {
+	//promise before loading page
+  let bl = new Promise((resolve) => {
+		blocker.once('request-blocked', (request) => {
+			resolve([url, false]);
+  	});
+	});
+
+  const page = await browser.newPage();
+  await blocker.enableBlockingInPage(page);
+  let pl = new Promise(async (resolve) => {
+		try {
+			await page.goto(url, { waitUntil: 'domcontentloaded', timeout:3000 });
+		} catch (e) {
+			resolve([url, "timed out"]);
+		}
+		resolve([url, true]);
+	});
+	let a = await Promise.race([pl, bl, new Promise(r => setTimeout(r([url, 'hf']), 1000))]);
+	//Why cant't r be async?	
+	await blocker.disableBlockingInPage(page);
+	await pl;
+	await page.close();
+
+	return a;
+}
+
+function getDomain(l) {
+  return l.split('\t')[4].split('.').reverse().join('.');
+}
+
+const file = readline.createInterface({
+    input: fs.createReadStream('../data/cc-hosts.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+let i = 0;
+for await (const l of file) {
+  break;
+}
+
+for await (const l of file) {
+  if (i == 5) break;
+  console.log(l);
+  console.log(await isLegit('https://' + getDomain(l)));
+  i++;
+}
+
+browser.close()
 

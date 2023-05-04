e7f71fb00cf956cad1de93e51b0387b28e64d9b9
teletrup <saletrapotasowatosyf@gmail.com>
2023-05-04 17:35:50 +0200

    naaaaaaa

--- util/page-sanitizer.py
+++ util/page-sanitizer.py
@@ -3,7 +3,6 @@ import json
 from tqdm import tqdm
 from lxml import html
 from lxml.html.clean import Cleaner
-from multiprocessing.pool import ThreadPool
 
 cleaner = Cleaner(page_structure=True,
                   meta=True,
@@ -16,33 +15,25 @@ cleaner = Cleaner(page_structure=True,
                   javascript=True,
                   comments=True)
 
-def task(l, pbar):
-  jni = json.loads(l)
-  body = jni['body']
-  print(1)
-  foo = html.fromstring(body)
-  title = ''.join(foo.xpath('//title/text()'))
-  cbody = cleaner.clean_html(foo).text_content().strip()
-  jno = {
-    'id': jni['id'],
-    'url': jni['url'],
-    'title': title,
-    'body': cbody,
-  }
-  pbar.update(len(l))
-  return jno
-
-def fgen(f):
-  for l in f:
-    yield l
-
 with open('../data/pages-raw.jsonl', 'r', 1000000000) as fi:
   fi.seek(0, io.SEEK_END)
   tot = fi.tell()
   fi.seek(0, io.SEEK_SET)
   with tqdm(total = tot) as pbar:
     with open('../data/pages.jsonl', 'w') as fo:
-      with ThreadPool(2) as pool:
-        for jno in pool.map(task, fgen(fi)):
-          fo.write(json.dumps(jno))
+      for l in fi:
+        jni = json.loads(l)
+        body = jni['body']
+        #print(body)
+        foo = html.fromstring(body)
+        title = ''.join(foo.xpath('//title/text()'))
+        cbody = cleaner.clean_html(foo).text_content().strip()
+        jno = {
+          'id': jni['id'],
+          'url': jni['url'],
+          'title': title,
+          'body': cbody,
+        }
+        fo.write(json.dumps(jno))
+        pbar.update(len(l))
       

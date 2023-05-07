c82da4681f55a4533c186ddf92a725d05b947598
teletrup <saletrapotasowatosyf@gmail.com>
2023-05-07 14:38:56 +0200

    bridge-client ui adjustments

--- search/search.js
+++ search/search.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
-
-
-
 const search = async q => {
   const url = `http://127.0.0.1:7280/api/v1/foo/search?query=${q}&snippet_fields=body`;
 	const response = await fetch(url);
@@ -21,7 +18,7 @@ app.get('/search', async (req, res) => {
   const sres = await search(req.query.q);
   const foo = sres.hits.map((r, i) => ({
     url: r.url,
-    title: r.title,
+    title: r.title ? r.title : 'Untitled',
     desc: sres.snippets[i].body,
   }));
   res.json(foo);
